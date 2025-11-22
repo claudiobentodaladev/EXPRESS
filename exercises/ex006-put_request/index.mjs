2ba96a741ec08c1278d511e798b7f27312346bb0
claudiobentodaladev <programmer.claudio@gmail.com>
2025-11-22 22:48:37 +0100

    REQUEST: put

---
+++ exercises/ex006-put_request/index.mjs
@@ -0,0 +1,34 @@
+import express from "express";
+
+const app = express()
+app.use(express.json())
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log("server is running!")
+})
+
+const users = [
+  { id: 1, name: "claudio", job: "developer" },
+  { id: 2, name: "bento", job: "nurse" },
+  { id: 3, name: "dala", job: "doctor" },
+];
+
+app.put('/api/users/:id', (req,res) => {
+    const {body, params: {id}} = req
+    const parsedId = parseInt(id)
+
+    if (isNaN(parsedId)) {
+        return res.status(400)
+    }
+
+    const indexUserFound = users.findIndex(user => user.id === parsedId)
+
+    if (indexUserFound === -1) {
+        return res.sendStatus(400)
+    }
+
+    users[indexUserFound] = {id: parsedId, ...body}
+
+    return res.sendStatus(200)
+})
