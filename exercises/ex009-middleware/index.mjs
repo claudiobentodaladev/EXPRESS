3d91c57f4f44fd68903e6113ba01dfba24404d6e
claudiobentodaladev <programmer.claudio@gmail.com>
2025-11-25 16:44:40 +0100

    MIDDLEWARE: normal use

---
+++ exercises/ex009-middleware/index.mjs
@@ -0,0 +1,43 @@
+import express from "express";
+
+const app = express()
+app.use(express.json())
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+// MIDDLEWARE 
+const handleID = (req, res, next) => {
+    const { params: { id } } = req
+    const parsedId = parseInt(id)
+
+    if (isNaN(parsedId)) {
+        return res.sendStatus(400)
+    }
+
+    const indexUserFound = users.findIndex(user => user.id === parsedId)
+
+    if (indexUserFound === -1) {
+        return res.sendStatus(404)
+    }
+
+    req.indexUserFound = indexUserFound;
+
+    next();
+}
+
+const users = [
+    { id: 1, name: "claudio", job: "developer" },
+    { id: 2, name: "bento", job: "nurse" },
+    { id: 3, name: "dala", job: "doctor" },
+];
+
+app.delete('/api/users/:id', handleID, (req, res) => {
+    const { indexUserFound } = req
+
+    users.splice(indexUserFound, 1)
+
+    return res.sendStatus(200)
+})
